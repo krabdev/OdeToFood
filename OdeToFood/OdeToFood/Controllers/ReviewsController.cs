f0b1f849ebb1381641d7bb5b02fd5f01c41fe68e
Andreas Oxinos <krabdev@icloud.com>
2013-03-20 02:38:46 +0200

    Restaurant, Reviews Controller added
    Added Restaurant controller through scaffolding, deleted eidt link and
    added review controller to be able to see CRUD reviews for restaurants.
    
    Started the review controller with the index view and a partial,

--- OdeToFood/OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/OdeToFood/Controllers/ReviewsController.cs
@@ -1,146 +1,33 @@
-﻿//using OdeToFood.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace OdeToFood.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
-//        [ChildActionOnly]
-//        public ActionResult BestReview()
-//        {
-//            var bestReview = from r in _reviews
-//                             orderby r.Rating descending
-//                             select r;
-//            return PartialView("_Review", bestReview.First());
-//        }
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
 
-//        //
-//        // GET: /Reviews/
+        //
+        // GET: /Reviews/
+        OdeToFoodDb _db = new OdeToFoodDb();
 
-//        public ActionResult Index()
-//        {
-//            var model =
-//                from r in _reviews
-//                orderby r.City
-//                select r;
+        public ActionResult Index([Bind(Prefix="id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
 
-//            return View(model);
-//        }
-
-//        //
-//        // GET: /Reviews/Details/5
-
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // GET: /Reviews/Create
-
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Create
-
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        //
-//        // GET: /Reviews/Edit/5
-
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            return View(review);
-//        }
-
-//        //
-//        // POST: /Reviews/Edit/5
-
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            if (TryUpdateModel(review))
-//            {
-//                //..
-//                return RedirectToAction("Index");
-//            }
-//            return View(review);
-//        }
-
-//        //
-//        // GET: /Reviews/Delete/5
-
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Delete/5
-
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview
-//            {
-//                Id = 1,
-//                Name = "Cinnamon Club",
-//                City = "Nicosia",
-//                Country = "Cyprus",
-//                Rating = 8
-//            },
-//            new RestaurantReview
-//            {
-//                Id = 2,
-//                Name = "Holiday Inn",
-//                City = "Limassol",
-//                Country = "Cyprus",
-//                Rating = 7
-//            },
-//            new RestaurantReview
-//            {
-//                Id = 3,
-//                Name = "Monaco",
-//                City = "London",
-//                Country = "France",
-//                Rating = 9
-//            }
-//        };
-//    }
-//}
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}

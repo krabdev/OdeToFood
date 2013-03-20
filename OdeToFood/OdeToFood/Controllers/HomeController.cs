11f3d706e5f2791b37384476654875921831815e
Andreas Oxinos <krabdev@icloud.com>
2013-03-20 23:56:58 +0200

    Finish controllers
    Ok, controllers finished, some ajax added for searching, paging added
    too (in home page)
    ajax is implemented through jquery insteed of ajax.beginForm
    
    Time for security

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace OdeToFood.Controllers
 {
@@ -11,18 +12,30 @@ namespace OdeToFood.Controllers
     {
         OdeToFoodDb _db = new OdeToFoodDb();
 
-        //serachTerm param cause a user might come to home page with a serrach result
-        public ActionResult Index(string searchTerm = null)
+        public ActionResult AutoComplete(string term)
         {
+            var model =
+                _db.Restaurants
+                    .Where(r => r.Name.StartsWith(term))
+                    .Take(10)
+                    .Select(r => new
+                    {
+                        label = r.Name
+                    });
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        //serachTerm param cause a user might come to home page with a serrach result
+        public ActionResult Index(string searchTerm = null, int page = 1)
+        {
+
 
             var model = 
                 _db.Restaurants
                     .OrderByDescending( r => r.Reviews.Average ( review => review.Rating))
-                    //.Take() and Skip() is used for pagination!
-                    
                     //Check if the serachTerm is null, if not make a serach
                     .Where ( r => searchTerm == null || r.Name.StartsWith(searchTerm))
-                    .Take(10)
                     .Select( r => 
                         new RestaurantListViewModel
                              { 
@@ -31,7 +44,7 @@ namespace OdeToFood.Controllers
                                  City =  r.City,
                                  Country = r.Country,
                                  CountOfReviews = r.Reviews.Count()
-                             });
+                             }).ToPagedList(page, 10);
 
             //If it's an ajax request from the search then just update the partial
             if (Request.IsAjaxRequest())
